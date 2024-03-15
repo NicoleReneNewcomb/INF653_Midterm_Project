5aaed88acebc9f505736ae1be084354e4c1f8c44
Nicole-Rene Newcomb <NicoleReneNewcomb@gmail.com>
2024-03-14 21:05:12 -0700

    Created Author model and read.php File
    Created the Author model and the read.php file to implement the "read all" GET request.

---
+++ api/authors/read.php
@@ -0,0 +1,52 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Author.php';
+
+    // Instantiate DB and connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate Author object
+    $author = new Author($db);
+
+    // Author read() query
+    $result = $author->read();
+    // count number of Authors
+    $num = $result->rowCount();
+
+    // Condition to check Authors exist
+    if ($num > 0) {
+
+        // Create Authors array
+        $authors_arr = array();
+        $authors_arr['data'] = array();
+
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+
+            // extract table fields to be used as variables, i.e. $id
+            extract($row);
+
+            // create individual Author array from extracted field variables
+            $author_item = array(
+                'id' => $id,
+                'author' => $author
+            );
+
+            // Push Author into "data" part of array
+            array_push($authors_arr['data'], $author_item);
+
+            // Turn to JSON and output
+            echo json_encode($authors_arr);
+        }
+    }
+    else {
+        
+        // If no posts
+        echo json_encode(
+            array('message' => 'author_id Not Found')
+        );
+    }
---
+++ models/Author.php
@@ -0,0 +1,157 @@
+<?php
+    class Author {
+
+        // DB Parameters
+        private $conn;
+        private $table = 'authors';
+
+        // Author Fields
+        public $id;
+        public $author;
+
+        // Database Constructor
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+        // Method read() to GET all Authors
+        public function read() {
+
+            // create SQL query
+            $query = 'SELECT
+                    p.id,
+                    p.author
+                FROM '
+                    . $this->table . ' p
+                ORDER BY
+                    p.created_at DESC';
+            
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // Execute query
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        // Method read_single() to GET single Author by ID
+        public function read_single() {
+            // create SQL query
+            $query = 'SELECT
+                    p.id,
+                    p.author
+                FROM '
+                    . $this->table . ' p
+                WHERE
+                    p.id = :id';
+            
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // Bind ID
+            $stmt->bindParam(':id', $this->id);
+
+            // Execute query
+            $stmt->execute();
+
+            // set Author object properties
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            $this->author = $row['author'];
+            
+            return $stmt;
+        }
+
+        // Method create() to POST new Author
+        public function create() {
+
+            // create SQL query
+            $query = 'INSERT INTO ' . 
+                    $this->table . '
+                (title, body, author, category_id, category_name)
+                VALUES
+                (:title, :body, :author, :category_id, :category_name)';
+
+            // prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // clean data
+            $this->author = htmlspecialchars(strip_tags($this->author));
+           
+            // bind data
+            $stmt->bindParam(':author', $this->author);
+            
+            // execute query
+            if ($stmt->execute()) {
+                return true;
+            }
+
+            // print error if something goes wrong
+            printf("Error: %s. \n", $stmt->error);
+
+            return false;
+        }
+
+        // Method update() to PUT changes to Author
+        public function update() {
+
+            // create SQL query
+            $query = 'UPDATE ' . 
+                    $this->table . '
+                SET
+                    author = :author,
+                WHERE 
+                    id =:id';
+            
+            // prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // clean data
+            $this->id = htmlspecialchars(strip_tags($this->id));
+            $this->author = htmlspecialchars(strip_tags($this->author));
+            
+            // bind data
+            $stmt->bindParam(':id', $this->id);
+            $stmt->bindParam(':author', $this->author);
+           
+            // execute query
+            if ($stmt->execute()) {
+                return true;
+            }
+
+            // print error if something goes wrong
+            printf("Error: %s. \n", $stmt->error);
+
+            return false;
+        }
+
+        // Method delete() to DELETE Author by ID
+        public function delete() {
+
+            // create SQL query
+            $query = 
+                'DELETE FROM ' .
+                    $this->table . '
+                WHERE
+                    id = :id';
+            
+            // prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // clean data
+            $this->id = htmlspecialchars(strip_tags($this->id));
+
+            // bind ID
+            $stmt->bindParam(':id', $this->id);
+
+            // execute query
+            if ($stmt->execute()) {
+                return true;
+            }
+
+            // print error if something goes wrong
+            printf("Error: %s. \n", $stmt->error);
+
+            return false;
+        }
+    }
