9042eb2569be5e5c655e61b8455dfe01a319b55a
Nicole-Rene Newcomb <NicoleReneNewcomb@gmail.com>
2024-03-15 02:34:15 -0700

    Updating CORS Policy
    Updating CORS policy settings.

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 declare(strict_types = 1);
 
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    exit();
+}
+
 echo '<pre>';
 print_r(getenv('SITE_URL'));
 echo '<br>';
